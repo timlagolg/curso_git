dce9efa61243d4e7c3ca62b42a208befdd78f6bc
Luan Gomes <luangomes_lima@hotmail.com>
2018-08-19 17:36:19 -0300

    Acrescentada uma modificação

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            
         }
     }
 }
